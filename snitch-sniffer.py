ce6253c3fe731d5542cb6fe680130a9bcd635109
rg-2 <robertcgough@gmail.com>
2021-01-08 14:30:14 -0500

    added something else

--- snitch-sniffer.py
+++ snitch-sniffer.py
@@ -3,6 +3,10 @@
 def snitch_sniffer():
     print('snitching sniff!!')
 
+def do_something_else():
+    print('doint something else!!!')
+
 if __name__ == '__main__':
     snitch_sniffer()
+    do_something_else()
 
