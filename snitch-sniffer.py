6ad7a718a08812d7c5b2c2bf505c6c27b8c5e6ba
rg-2 <robertcgough@gmail.com>
2021-01-08 14:47:56 -0500

    added lidar

--- snitch-sniffer.py
+++ snitch-sniffer.py
@@ -6,7 +6,12 @@ def snitch_sniffer():
 def do_something_else():
     print('doing something else, that was added back in!!!')
 
+def do_lidar():
+    print('doing lidar')
+
 if __name__ == '__main__':
     snitch_sniffer()
     do_something_else()
+
+    do_lidar()
 
